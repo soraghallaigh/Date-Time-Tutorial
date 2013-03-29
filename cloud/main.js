822c8a0ab6bf2632c0fce81a434643b3396af737
cianclarke <cian@cianclarke.com>
2013-03-29 10:30:31 -0400

    Use JSDom to screen scrape the current date & time div on the cloud side & return the string to the client

--- cloud/main.js
+++ cloud/main.js
@@ -1,9 +1,19 @@
 var util = require('util');
 var request = require('request');
+var jsdom = require("jsdom");
 
 exports.getCurrentTime = function(params, cb) {
 
   request({uri : 'http://www.timeanddate.com/worldclock/city.html?n=78'}, function(err, res, body){
-    return cb(err, { response : body });
+    // Run some jQuery on a html fragment
+    jsdom.env(
+    body,
+    ["http://code.jquery.com/jquery.js"],
+    function(errors, window) {
+      var ct = window.$('#ct').text();
+      console.log("contents of the current time div:", ct);
+      return cb(errors, { response : ct });
+    }
+    );
   });
 };
