f06dadfd5d01db997d06e84590d011de49cde7f5
fhdemo2@example.com <fhdemo2@example.com>
2013-12-11 20:05:04 +0000

    update file /cloud/main.js

--- cloud/main.js
+++ cloud/main.js
@@ -18,7 +18,7 @@ exports.getCurrentTime = function(params, cb) {
  cache isn't 'hot'
  */
 function getDateTimeFromWeb(params, cb){
-  request({uri : 'http://www.timeanddate.com/worldclock/city.html?n=78'}, function(err, res, body){
+  request({uri : 'ttp://www.timeanddate.com/worldclock/city.html?n=78'}, function(err, res, body){
     // Run some jQuery on a html fragment
     jsdom.env(
     body,
